cb1ce402130ca775b6d9f643fc7adccd0ed64102
Marcella <marcellaacoelho@gmail.com>
2025-06-04 09:01:27 -0300

    first commit

---
+++ site-uniesp/src/pages/VisualizaNoticia.jsx
@@ -0,0 +1,32 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { Container, Card } from 'react-bootstrap'
+import { useParams } from 'react-router-dom'
+
+const VisualizaNoticia = () => {
+
+    const { id } = useParams()
+    const [noticia, setNoticia] = useState({})
+    const urlBase = "http://localhost:3000"
+
+    useEffect(() => {
+        axios.get(`${urlBase}/noticias/${id}`)
+        .then(response => setNoticia(response.data))
+        .catch(error => console.error("Erro ao obter a noticia: ", error))
+    }, [id])
+
+  return (
+    <Container className="py-4">
+        <Card className="shadow-sm">
+            <Card.Body>
+                <Card.Title>{noticia.titulo}</Card.Title>
+                <Card.Text>{noticia.texto}</Card.Text>
+            </Card.Body>
+
+        </Card>
+
+    </Container>
+  )
+}
+
+export default VisualizaNoticia
